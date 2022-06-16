dd21be875812a57d124b1ca487baeea309f17897
Pinto Manuel <pintoydc@gmail.com>
2022-06-16 05:52:56 +0200

    loader added

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -6,9 +6,11 @@ const Banner = () => {
    /*  const [imageUrl, setImageUrl] = useState(""); */
     const { data: dataS, isLoading, isSuccess, isError } = useGetTopArtistQuery("")
 
-    /* console.log(data); */
+
     
-    console.log(dataS?.data.map((x) => x.album));
+    /* console.log(dataS?.data.map((x) => x.album)); */
+    console.log(dataS);
+    
 
 
 
@@ -33,17 +35,20 @@ const Banner = () => {
       <div className="container min-w-full bg-smooth-black mx-auto py-9 md:py-12 px-4 mb-8 -mt-4 md:px-6">
           <div className="flex items-strech justify-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 lg:space-x-8">
               <div className="flex flex-col md:flex-row items-strech justify-between bg-gray-50 py-6 px-6 md:py-12 lg:px-12 md:w-8/12 lg:w-7/12 xl:w-8/12 2xl:w-9/12">
-                  <div className="flex flex-col justify-center md:w-1/2">
+                  <div className="flex flex-col  md:flex-row md:min-h-full">
                       <h1 className="text-3xl lg:text-4xl text-white  font-semibold text-gray-800">Best Deal</h1>
                       <p className="text-base lg:text-xl text-gray-800 text-white mt-2">
                           Save upto <span className="font-bold">50%</span>
                       </p>
                   </div>
-                 
-                      {dataS?.data.slice(0, 1).map((x) => (
-                          <div key={x.id} className="">
-                              <div className="md:w-1/2  mt-8 md:mt-0 flex justify-center md:justify-end">
-                                  <img  src={x.album.cover_big} alt="img" />
+                      {dataS?.data.slice(0, 3).map((x) => (
+                          <div key={x.id}>
+                              <div className="md:w-1/2  md:mt-0 flex w-full mx-auto justify-center md:justify-end">
+                                  <div className=" ">
+                                      <img src={x.album.cover_medium} alt="img" />
+                                      <span className="  mx-auto text-white md:gap-8 sm:gap-8 "><span className="font-extrabold ">Name</span> <h1>{x.artist.name}</h1></span>
+                                      <p className="text-white text-start mx-auto text-xs mt-1"><span className=" font-extrabold text-sm">Title:</span> {x.album.title}</p>
+                                  </div>
                               </div>
                           </div>
                       ))}
--- src/components/homeCard/HomeCard.jsx
+++ src/components/homeCard/HomeCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { IoOptionsOutline } from "react-icons/io5";
 import { useGetAlbumsQuery } from '../../services/AllApi'
 import Banner from '../banner/Banner';
+import loader from './../../assets/loading.gif';
 
 const HomeCard = () => {
     const { data: dbt, isSuccess, isLoading, isFetching, error } = useGetAlbumsQuery("")
@@ -10,12 +11,12 @@ const HomeCard = () => {
     <div>
           <Banner />
           <div>
-              {isLoading && <h2>Loading data...</h2>}
-              {error && <h2>Something went wrong, please try again later...</h2>}
+              {isLoading && <img className=" flex mx-auto items-center -my-60 absolute" src={loader} alt="loading" /> }
+              {error && <h2 className='text-white text-center'>Upss.. something went wrong, please try again later...</h2>}
               {dbt?.data.slice(0, 1).map((icon) => (
                   <div key={icon.id}>
-                      <div className="flext text-white md:text-left text-center text-2xl capitalize mb-8 mx-auto">
-                          <IoOptionsOutline className="mt-2 ml-2 md:text-left flex text-center" />
+                      <div className="flext my-20 text-white md:text-left text-center text-2xl capitalize mb-8 mx-auto">
+                          <IoOptionsOutline className=" ml-40 -my-8 md:text-left flex text-center" />
                           New Release
                       </div>
                   </div>
@@ -23,8 +24,11 @@ const HomeCard = () => {
           </div>
           {/* end of the heading */}
 
+
+
           {/* Card */}
           <div>
+              {/* responsive card */}
               <div className="container flex w-full mx-auto min-h-full py-9 md:py-12 px-4 mb-8 -mt-4 md:px-6">
                   {isSuccess && (
                       <div className="flex flex-col  md:flex-row md:min-h-full   gap-5 mx-auto ">
