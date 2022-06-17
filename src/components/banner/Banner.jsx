081030c6e41e690464801c14a1a1380f31b5f48b
Pinto Manuel <pintoydc@gmail.com>
2022-06-17 04:27:04 +0200

    commit done

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -26,7 +26,7 @@ const Banner = () => {
 
                           <div>
                               <div className="md:w-1/2 gap-3 md:mt-0 flex w-fit mx-auto justify-center md:justify-end">
-                                  {dataTopArtist?.data.slice(0, 4).map((itemsVideo) => (
+                                  {dataTopArtist?.data.slice(0, 2).map((itemsVideo) => (
                                       <div key={itemsVideo.id}>
                                           <img className="text-white w-fit" src={itemsVideo.album.cover_big} alt="Image" />
                                           <span className="text-white grid justify-center">{`Artist: ${itemsVideo.artist.name.substring(0, 10)}`}</span>
