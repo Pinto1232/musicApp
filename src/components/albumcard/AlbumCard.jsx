0681d5c6523d67d1d0c1b5950c9c68e27e973fd5
Pinto Manuel <pintoydc@gmail.com>
2022-06-21 06:00:55 +0200

    environment created

--- src/components/albumcard/AlbumCard.jsx
+++ src/components/albumcard/AlbumCard.jsx
@@ -18,7 +18,6 @@ const AlbumCard = (props) => {
           <Grid container>
           <div className="grid grid-cols-1 xs:grid-cols-2   sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-7 mx-auto mt-10  p-4 gap-8">
               {error && <p className='text-white text-center text-lg'>Something went wrong, please try again later...</p>}
-             
               {relatedData?.data.slice(0, 14).map((items) => (
                   <Box key={items.id}  sx={{ width: 210, marginRight: 0.5, my: 5 }}>
                       <div className=" Card  text-white bg-smooth-black w-full">
@@ -39,7 +38,8 @@ const AlbumCard = (props) => {
                               )}
                               
                           <Box
-                              className="flex items-center justify-between leading-tight p-2 md:p-4">
+                              className="flex items-center 
+                              justify-between leading-tight p-2 md:p-4">
                               {items ? (
                                   <Box sx={{pr: 2}}>
                                     <Typography gutterBottom variant="body2">
@@ -56,8 +56,6 @@ const AlbumCard = (props) => {
                                     <Skeleton width="60%" />
                                     </Box>    
                               )} 
-         
-    
                           </Box>
                           <Box className="flex items-center justify-between bg-blue leading-none p-2 md:p-4">
                               <a className="flex items-center no-underline hover:underline text-white" href="#">
